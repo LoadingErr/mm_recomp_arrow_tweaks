68377fe653d14032a2886abff215d5de3649d6d2
LT-Schmiddy <schmidalex1996@gmail.com>
2025-05-15 12:13:25 -0400

    general refactors.

--- src/dpad.c
+++ src/dpad.c
@@ -14,67 +14,9 @@
 #include "z64horse.h"
 #include "z64player.h"
 
-struct EnArrow;
+#define ARROW_LIMB_MAX 5
 
-typedef void (*EnArrowActionFunc)(struct EnArrow*, PlayState*);
-
-typedef enum ArrowType {
-    /* 0 */ ARROW_TYPE_NORMAL_LIT, // Normal arrow lit on fire
-    /* 1 */ ARROW_TYPE_NORMAL_HORSE, // Normal arrow shot while riding a horse
-    /* 2 */ ARROW_TYPE_NORMAL,
-    /* 3 */ ARROW_TYPE_FIRE,
-    /* 4 */ ARROW_TYPE_ICE,
-    /* 5 */ ARROW_TYPE_LIGHT,
-    /* 6 */ ARROW_TYPE_SLINGSHOT,
-    /* 7 */ ARROW_TYPE_DEKU_BUBBLE,
-    /* 8 */ ARROW_TYPE_DEKU_NUT
-} ArrowType;
-
-#define ARROW_IS_MAGICAL(arrowType) (((arrowType) >= ARROW_TYPE_FIRE) && ((arrowType) <= ARROW_TYPE_LIGHT))
-#define ARROW_GET_MAGIC_FROM_TYPE(arrowType) (s32)((arrowType) - ARROW_TYPE_FIRE)
-#define ARROW_IS_ARROW(arrowType) ((arrowType) < ARROW_TYPE_SLINGSHOT)
-
-typedef enum ArrowMagic {
-    /* -1 */ ARROW_MAGIC_INVALID = -1,
-    /*  0 */ ARROW_MAGIC_FIRE = ARROW_GET_MAGIC_FROM_TYPE(ARROW_TYPE_FIRE),
-    /*  1 */ ARROW_MAGIC_ICE = ARROW_GET_MAGIC_FROM_TYPE(ARROW_TYPE_ICE),
-    /*  2 */ ARROW_MAGIC_LIGHT = ARROW_GET_MAGIC_FROM_TYPE(ARROW_TYPE_LIGHT),
-    /*  3 */ ARROW_MAGIC_DEKU_BUBBLE // Only used in Player. Does not map to ARROW_TYPE_SLINGSHOT
-} ArrowMagic;
-
-
-typedef struct {
-    /* 0x144 */ SkelAnime skelAnime;
-} EnArrowArrow; // size = 0x1A8
-
-typedef struct {
-    /* 0x144 */ f32 unk_144;
-    /* 0x148 */ u8 unk_148;
-    /* 0x149 */ s8 unk_149;
-    /* 0x14A */ s16 unk_14A;
-    /* 0x14C */ s16 unk_14C;
-} EnArrowBubble; // size = 0x150
-
-typedef struct EnArrow {
-    /* 0x000 */ Actor actor;
-    union {
-        EnArrowArrow arrow;
-        EnArrowBubble bubble;
-    };
-    /* 0x1A8 */ ColliderQuad collider;
-    /* 0x228 */ Vec3f unk_228;
-    /* 0x234 */ Vec3f unk_234;
-    /* 0x240 */ s32 unk_240;
-    /* 0x244 */ WeaponInfo unk_244;
-    /* 0x260 */ u8 unk_260; // timer in OoT
-    /* 0x261 */ u8 unk_261; // hitFlags in OoT
-    /* 0x262 */ u8 unk_262;
-    /* 0x263 */ u8 unk_263;
-    /* 0x264 */ Actor* unk_264;
-    /* 0x268 */ Vec3f unk_268;
-    /* 0x274 */ EnArrowActionFunc actionFunc;
-} EnArrow; // size = 0x278
-
+#include "overlays/actors/ovl_En_Arrow/z_en_arrow.h"
 
 typedef enum {
     DPAD_USAGE_MODE_NONE,
@@ -1978,7 +1920,7 @@ bool Player_IsAiming(Player* this, PlayState* play) {
              (this->upperActionFunc == Player_UpperAction_8 ||
               this->upperActionFunc == Player_UpperAction_7));
 }
-
+/*
 // Handle arrow cycling
 void CycleArrowsL(Player* this, PlayState* play, Input* input) {
     EquipSlot bowButton = EQUIP_SLOT_NONE;
@@ -2072,9 +2014,9 @@ void CycleArrowsL(Player* this, PlayState* play, Input* input) {
             }
         }
     }
-}
+}*/
 
-void CycleArrowsR(Player* this, PlayState* play, Input* input) {
+void CycleArrows(Player* this, PlayState* play, Input* input, bool using_r) {
     EquipSlot bowButton = EQUIP_SLOT_NONE;
 
     // Find button equipped with bow
@@ -2096,7 +2038,7 @@ void CycleArrowsR(Player* this, PlayState* play, Input* input) {
     u8 previousBowItem = gSaveContext.save.saveInfo.equips.buttonItems[CUR_FORM][bowButton];
 
     // Check for R press
-    if (CHECK_BTN_ALL(input->press.button, BTN_R)) {
+    if (CHECK_BTN_ALL(input->press.button, using_r ? BTN_R : BTN_L)) {
         do {
             currentArrowIndex++;
 
@@ -2241,11 +2183,11 @@ RECOMP_HOOK("Player_UpdateCommon") void ArrowCycleHandle(Player* this, PlayState
     if (Player_IsAiming(this, play) && 
         !Player_IsHoldingHookshot(this)  && 
         (CFG_CYCLING_MODE == CYCLING_MODE_L)) {
-            CycleArrowsL(this, play, input);
+            CycleArrows(this, play, input, false);
     } else if (Player_IsAiming(this, play) &&
         !Player_IsHoldingHookshot(this)  && 
         (CFG_CYCLING_MODE == CYCLING_MODE_R)) {
-            CycleArrowsR(this, play, input);
+            CycleArrows(this, play, input, true);
     }
 }
 
